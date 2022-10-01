e778701acc65769d036cbdf85fe96db75e1c7d48
Alioune badara dieng <badaralune9@gmail.com>
2022-10-01 21:48:14 +0100

    Update

--- app/GraphQL/Type/LocataireType.php
+++ app/GraphQL/Type/LocataireType.php
@@ -2,7 +2,7 @@
 
 namespace App\GraphQL\Type;
 
-use  App\Models\{Locataire};
+use App\Models\{Locataire};
 use Carbon\Carbon;
 use GraphQL\Type\Definition\Type;
 use Rebing\GraphQL\Support\Type as GraphQLType;
--- app/Models/Outil.php
+++ app/Models/Outil.php
@@ -23,7 +23,7 @@ class Outil extends Model
 
     public static $queries = array(
         "proprietaires"              => " id,code,nom,prenom,telephone,agence_id,agence{id,nom_agence},biens{id,code,adresse,description}",
-        "locataires"                 => " id,code,nom,prenom,telephone,bien_immo_id,bien_immo{id,code,description,montant,proprietaire_id,proprietaire{id,code,nom,prenom,telephone,agence_id,agence{id,nom_agence}}}",
+        "locataires"                 => " id,code,nom,prenom,telephone,bien_immo_id,bien_immo{id,code,description,loyer,proprietaire_id,proprietaire{id,code,nom,prenom,telephone,agence_id,agence{id,nom_agence}}}",
         "users"                      => " id,name,email,role{id,nom}",
         "bien_immos"                 => " id,code,adresse,description,loyer,locataire{id,code,nom,prenom,telephone}",
         "biens"                      => " id,code,adresse,description,proprietaire_id,proprietaire{id,code,nom,prenom,telephone,agence_id,agence{id,nom_agence}},type_bien_immo{id,nom}",
